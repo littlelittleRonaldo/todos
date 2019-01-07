3a91681f4cf95cbf2650a0d3fa933c930e231dd1
xk <xiakai@360fdc.com>
2019-01-07 11:29:45 +0800

    应好友要求加了点无聊的翻译

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ var vm = new Vue ({
         list:localStorage_todos.get(),
         updated:[],
         editedDesc:'',
-        isSelected:'all'
+        isSelected:'all',
+        isEn: true
     },
     computed:{
         allDone: {
@@ -53,6 +54,45 @@ var vm = new Vue ({
                     return item.isCompleted;
                 })
             }
+        },
+        content(){
+            if (this.isEn) {
+                return {
+                    title: 'todos',
+                    word_1: 'What needs to be done?',
+                    word_2: 'item left',
+                    word_3: 'All',
+                    word_4: 'Active',
+                    word_5: 'Completed',
+                    word_6: 'Clear completed',
+                    word_7: 'Double-click to edit a todo',
+                    word_8: 'Template by ',
+                    word_9: 'Sindre Sorhus',
+                    word_10: 'Created by ',
+                    word_11: 'shark',
+                    word_12: 'Part of ',
+                    word_13: 'TodoMVC',
+                    word_14: '如果你只认得我,点我!'
+                }
+            }else {
+                return {
+                    title: '土豆丝',
+                    word_1: '还有么四要奏滴?',
+                    word_2: '过还冒奏',
+                    word_3: '全部',
+                    word_4: '要奏冒奏滴',
+                    word_5: '奏完了滴',
+                    word_6: '去掉奏完滴',
+                    word_7: '双击666,不是,是双击编辑',
+                    word_8: '模板来自 ',
+                    word_9: '辛德罗 索罗斯',
+                    word_10: '作者是 ',
+                    word_11: '西崖客',
+                    word_12: '来自 ',
+                    word_13: 'TodoMVC',
+                    word_14: 'If you only recognize me, click on me!'
+                }
+            }
         }
     },
     methods:{
@@ -86,6 +126,9 @@ var vm = new Vue ({
         },
         filterList(val){
             this.isSelected = val;
+        },
+        toggleLang(){
+            this.isEn = !this.isEn;
         }
     },
     directives:{
